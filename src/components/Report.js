c58d470d3fedecc70af0e03dd94e106b95450189
Daniel Olaszy <daniel.olaszy@gmail.com>
2022-04-14 18:28:39 +0100

    Createtd function to handle form submit and send post request

--- src/components/Report.js
+++ src/components/Report.js
@@ -1,53 +1,70 @@
 import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const Report = () => {
+  const [location, setLocation] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [status, setStatus] = useState("pending");
+
+  const reportCrime = async (e) => {
+    e.preventDefault();
+    axios.post("http://127.0.0.1:8000/api/crimereports/", {
+      "location": location,
+      "description": description,
+      "categories": category,
+      "status": status,
+    });
+  };
+
   return (
     <>
       <div className="container bg-light p-5 rounded-3 mt-5">
-        <form class="row row-cols-6 g-3 align-items-center">
-          <div class="col-12">
+        <form className="row row-cols-6 g-3 align-items-center" onSubmit={reportCrime}>
+          <div className="col-12">
             <h3 className="m-0">Report a Crime</h3>
           </div>
 
-          <div class="col-12">
-            <label for="inputCategory" class="form-label">
+          <div className="col-12">
+            <label htmlFor="inputCategory" className="form-label">
               Category
             </label>
-            <select id="inputCategory" class="form-select">
-              <option selected>Choose...</option>
-              <option>Anti-social behaviour</option>
-              <option>Bicycle theft</option>
-              <option>Burglary</option>
-              <option>Criminal damage and arson</option>
-              <option>Drugs</option>
-              <option>Other theft</option>
-              <option>Possession of weapons</option>
-              <option>Public order</option>
-              <option>Robbery</option>
-              <option>Shoplifting</option>
-              <option>Theft from the person</option>
-              <option>Vehicle crime</option>
-              <option>Violence and sexual offences</option>
-              <option>Other crime</option>
+            <select id="inputCategory" className="form-select" onChange={(e) => setCategory(e.target.value)}>
+              <option value="">Choose...</option>
+              <option value="anti-social-behaviour">Anti-social behaviour</option>
+              <option value="bicycle-theft">Bicycle theft</option>
+              <option value="burglary">Burglary</option>
+              <option value="criminal-damage-arson">Criminal damage and arson</option>
+              <option value="drugs">Drugs</option>
+              <option value="other-theft">Other theft</option>
+              <option value="possession-of-weapons">Possession of weapons</option>
+              <option value="public-order">Public order</option>
+              <option value="robbery">Robbery</option>
+              <option value="shoplifting">Shoplifting</option>
+              <option value="theft-from-the-person">Theft from the person</option>
+              <option value="vehicle-crime">Vehicle crime</option>
+              <option value="violent-crime">Violence and sexual offences</option>
+              <option value="other-crime">Other crime</option>
             </select>
           </div>
 
-          <div class="col-12">
-            <label for="inputAddress" class="form-label">
-              Street
+          <div className="col-12">
+            <label htmlFor="inputAddress" className="form-label">
+              Location
             </label>
-            <input type="text" class="form-control" id="inputAddress" placeholder="Location of the crime" />
+            <input type="text" className="form-control" id="inputAddress" placeholder="Location of the crime" onChange={(e) => setLocation(e.target.value)} />
           </div>
 
-          <div class="col-12">
-            <label for="inputDescription" class="form-label">
+          <div className="col-12">
+            <label htmlFor="inputDescription" className="form-label">
               Description
             </label>
-            <input type="text" class="form-control" id="inputDescription" placeholder="Description of the crime" />
+            <input type="text" className="form-control" id="inputDescription" placeholder="Description of the crime" onChange={(event) => setDescription(event.target.value)} />
           </div>
 
-          <div class="col-12">
-            <button type="submit" class="btn btn-primary">
+          <div className="col-12">
+            <button type="submit" className="btn btn-primary">
               Report
             </button>
           </div>
