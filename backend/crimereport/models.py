14ee0d5f902cde28f0116d72988d37518b6e93a2
zortol <nepal_14221@hotmail.com>
2022-04-15 17:03:53 +0100

    Added crime_date model, added validator for crime_date and altered categories name

--- backend/crimereport/models.py
+++ backend/crimereport/models.py
@@ -1,10 +1,24 @@
 from sre_constants import CATEGORY
 from django.db import models
 from django.utils import timezone
+import datetime
+from django.core.exceptions import ValidationError
+from django.core.validators import MaxValueValidator
+
+
 
 
 # Create your models here.
 class Crimereport(models.Model):
+
+    def __str__(self):
+        return self.description
+        
+    def check_date(crime_date):
+        if crime_date > datetime.date.today() == True:
+            raise ValidationError("The date cannot be in the future!")
+
+
     # first element in each tuple is the actual value to be set on the model, and the second element is the human-readable name
     CATEGORY_CHOICES= (
         ("anti-social-behaviour","Anti-social behaviour"),
@@ -33,12 +47,19 @@ class Crimereport(models.Model):
     modified = models.DateTimeField(editable=False) # intially was month in the 'uml' diagram
     status = models.CharField(max_length=120, default="pending",blank=True)    # status should be hidden in the frontend since this is controlled by admin
     categories = models.CharField(max_length = 100, choices = CATEGORY_CHOICES, default="Picking option")
+    
 
 
+    # format of the 
+    crime_date = models.DateField(validators=[MaxValueValidator(datetime.date.today())])
+
+
+        
 
     def save(self, *args, **kwargs):
         ''' On save, update timestamps '''
         if not self.id:
             self.created = timezone.now()
         self.modified = timezone.now()
+
         return super(Crimereport, self).save(*args, **kwargs)
--- src/components/Report.js
+++ src/components/Report.js
@@ -1,16 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-import { useState, useEffect } from "react";
-
-const Report = () => {
-  const [isoDate, setIsoDate] = useState(new Date().toISOString().slice(0, 10));
-  const [localDate, setLocalDate] = useState(new Date().toLocaleDateString());
-
-  useEffect(() => {
-    console.log(isoDate);
-    console.log(localDate);
-  }, []);
-=======
 import axios from "axios";
 import { useState, useEffect } from "react";
 
@@ -29,16 +17,11 @@ const Report = () => {
       "status": status,
     });
   };
->>>>>>> c58d470d3fedecc70af0e03dd94e106b95450189
 
   return (
     <>
       <div className="container bg-light p-5 rounded-3 mt-5">
-<<<<<<< HEAD
-        <form className="row row-cols-6 g-3 align-items-center">
-=======
         <form className="row row-cols-6 g-3 align-items-center" onSubmit={reportCrime}>
->>>>>>> c58d470d3fedecc70af0e03dd94e106b95450189
           <div className="col-12">
             <h3 className="m-0">Report a Crime</h3>
           </div>
@@ -47,15 +30,8 @@ const Report = () => {
             <label htmlFor="inputCategory" className="form-label">
               Category
             </label>
-<<<<<<< HEAD
-            <select id="inputCategory" className="form-select">
-              <option value="Choose..." selected>
-                Choose...
-              </option>
-=======
             <select id="inputCategory" className="form-select" onChange={(e) => setCategory(e.target.value)}>
               <option value="">Choose...</option>
->>>>>>> c58d470d3fedecc70af0e03dd94e106b95450189
               <option value="anti-social-behaviour">Anti-social behaviour</option>
               <option value="bicycle-theft">Bicycle theft</option>
               <option value="burglary">Burglary</option>
@@ -75,26 +51,16 @@ const Report = () => {
 
           <div className="col-12">
             <label htmlFor="inputAddress" className="form-label">
-<<<<<<< HEAD
-              Street
-            </label>
-            <input type="text" className="form-control" id="inputAddress" placeholder="Location of the crime" />
-=======
               Location
             </label>
             <input type="text" className="form-control" id="inputAddress" placeholder="Location of the crime" onChange={(e) => setLocation(e.target.value)} />
->>>>>>> c58d470d3fedecc70af0e03dd94e106b95450189
           </div>
 
           <div className="col-12">
             <label htmlFor="inputDescription" className="form-label">
               Description
             </label>
-<<<<<<< HEAD
-            <input type="text" className="form-control" id="inputDescription" placeholder="Description of the crime" />
-=======
             <input type="text" className="form-control" id="inputDescription" placeholder="Description of the crime" onChange={(event) => setDescription(event.target.value)} />
->>>>>>> c58d470d3fedecc70af0e03dd94e106b95450189
           </div>
 
           <div className="col-12">
