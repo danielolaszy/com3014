a3c851ab11b1d5c649c833d83c9e9056704935b4
zortol <nepal_14221@hotmail.com>
2022-04-15 15:12:38 +0100

    Pulled from main first

--- backend/crimereport/models.py
+++ backend/crimereport/models.py
@@ -7,20 +7,20 @@ from django.utils import timezone
 class Crimereport(models.Model):
     # first element in each tuple is the actual value to be set on the model, and the second element is the human-readable name
     CATEGORY_CHOICES= (
-        ("Anti-social behaviour","Anti-social behaviour"),
-        ("Bicycle-theft","Bicycle-theft"),
-        ("Burglary","Burglary"),
-        ("Criminal damage and arson","Criminal damage and arson"),
-        ("Drugs","Drugs"),
-        ("Other theft","Other theft"),
-        ("Possession of weapons","Possession of weapons"),
-        ("Public order","Public order"),
-        ("Robbery","Robbery"),
-        ("Shoplifting","Shoplifting"),
-        ("Theft from the person","Theft from the person"),
-        ("Vehicle crime","Vehicle crime"),
-        ("Violence and sexual offences","Violence and sexual offences"),
-        ("Other crime","Other crime"),
+        ("anti-social-behaviour","Anti-social behaviour"),
+        ("bicycle-theft","Bicycle-theft"),
+        ("burglary","Burglary"),
+        ("criminal-damage-arson","Criminal damage and arson"),
+        ("drugs","Drugs"),
+        ("other-theft","Other theft"),
+        ("possession-of-weapons","Possession of weapons"),
+        ("public-order","Public order"),
+        ("robbery","Robbery"),
+        ("shoplifting","Shoplifting"),
+        ("theft-from-the-person","Theft from the person"),
+        ("vehicle-crime","Vehicle crime"),
+        ("violent-crime","Violence and sexual offences"),
+        ("other-crime","Other crime"),
         
     )
 
@@ -31,7 +31,7 @@ class Crimereport(models.Model):
     ## created and modified are fields that are created and modified by django not by the user
     created = models.DateTimeField(editable=False)  # intially was month in the 'uml' diagram 
     modified = models.DateTimeField(editable=False) # intially was month in the 'uml' diagram
-    status = models.CharField(max_length=120, default="pending")    # status should be hidden in the frontend since this is controlled by admin
+    status = models.CharField(max_length=120, default="pending",blank=True)    # status should be hidden in the frontend since this is controlled by admin
     categories = models.CharField(max_length = 100, choices = CATEGORY_CHOICES, default="Picking option")
 
 
