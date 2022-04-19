0b0c0e2fe24404c314f001bf251bed5bd6d7d8e3
zortol <nepal_14221@hotmail.com>
2022-04-19 09:59:40 +0100

    Added crime_date to serializers

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -84,6 +84,20 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'sql_server.pyodbc',
+#         'NAME': 'CrimeReport',
+#         'USER': 'webtech03',
+#         'PASSWORD': 'webtech@03',
+#         'HOST': 'webtechgroup3.database.windows.net',
+#         'PORT': '1433',
+
+#         'OPTIONS': {
+#             'driver':'ODBC Driver 13 for SQL Server',
+#         },
+#     },
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
