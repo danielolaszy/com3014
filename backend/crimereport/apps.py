a3293426566d52188f8bdb3b6a64f9a7f2c166c6
zortol <nepal_14221@hotmail.com>
2022-04-28 13:31:48 +0100

    Remove the now ignored directory 'backend and vir_env'

--- backend/crimereport/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CrimereportConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'crimereport'
--- backend/crimereport/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .serializers import CrimereportSerializer
-from .models import Crimereport
-
-# Create your views here.
-class CrimereportView(viewsets.ModelViewSet):
-    serializer_class = CrimereportSerializer
-    queryset = Crimereport.objects.all()
